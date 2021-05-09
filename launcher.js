4fdfc3c07b7cca887fb153ee680930b7cf0a1c97
tia-sharma <77229942+tia-sharma@users.noreply.github.com>
2021-05-09 11:44:46 +0100

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,29 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 5
+        }
+        this.bodyA=bodyA;
+        this.pointB=pointB;
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    display(){
+        if(this.launcher.bodyA) {
+        var pointA = this.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
+
